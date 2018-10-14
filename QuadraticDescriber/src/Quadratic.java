937c48d6b2e638d8c11a7095a2ef19ab13226e6e
bryanvtam <42446827+bryanvtam@users.noreply.github.com>
2018-10-13 21:41:46 -0700

    finished the quadratic function, need to add the while loop for the "quit"

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -1,53 +1,62 @@
-
+import java.util.*;
 public class Quadratic {
-
 	public static String quadrDescriber(double a, double b, double c) {
-		System.out.println("===========================================================================================================");
-		System.out.println("a: " + a);
-		System.out.println("b: " + b);
-		System.out.println("c: " + c);
-		System.out.println("Description of the graph of: ");
-		System.out.println();
+
+		//variables
+		Scanner userinput = new Scanner(System.in);
+		double symmetry = 0;
+		double vertexX = 0;
+		double vertexY = 0;
+		double x_subtract = 0;
+		double x_addition = 0;
+		String xintercepts = "";
+		double yIntercept = 0;
+		String opens = "";
+	
+		//user input for variables a, b, c
+		System.out.print("a :" );
+		a = userinput.nextInt();
+		System.out.print("b :" );
+		b = userinput.nextInt();
+		System.out.print("c :" );
+		c = userinput.nextInt();
+		
 		//determines if it opens up or down
-		if (a > 0) {
-			System.out.println("Opens: down");
+		if (a < 0) {
+			opens = "Down";
 		}
 		else {
-			System.out.print("Opens: Up");
+			opens = "Up";
 		}
-		//finding the axis of symmetry
-		double symmetry = (-(b)/(2*a));
-		System.out.println("Axis of Symmetry: " + symmetry);
-		//finding the vertex
-		double vertexX = symmetry;
-		double vertexY = ((a)*(vertexX*vertexX) + (b)*(vertexX) + c);
-		System.out.println ("Vertex: (" + vertexX + "," + vertexY + ")");
-		//find the discriminate for the x-intercept 
-		double discriminate = ((b*b) - 4*(a)*(c));
-		//square root of discriminate
-		double t = 1;
-		double sqrt = 0;
-		double squareroot = discriminate/2;
-		do 
-		{
-			t = squareroot;
-			squareroot = ((t+ (discriminate/t))/2);
-		}
-		while ((t-squareroot) != 0);
-		{
-			sqrt = t;
-		}
-		//use the quadratic formula to find the x intercepts
-		double negativeZero= ((-b + sqrt)/(2*a) );
-		double positiveZero = ((-b - sqrt)/(2*a));
-		System.out.println("x-intercept(s) : " + positiveZero  + " & " + negativeZero);
-		//finding the y-intercept
-		double yIntercept = c;
-		System.out.println("y-intercept: (0," + c + ")");
 		
-		System.out.println("===========================================================================================================");
-		System.out.println("Do you want to keep going? (Type \"quit\" to end)");
-		//add scanners in to check the user input
-		//make the user input all capitals or all lower case in order to check the string
-	}
+		//finding the axis of symmetry
+		symmetry = (-(b)/(2*a));
+		
+		//finding the vertex
+		vertexX = symmetry;
+		vertexY = ((a)*(vertexX*vertexX) + (b)*(vertexX) + c);
+			
+		//find the x-intercept(s) 
+		x_subtract = ((Math.sqrt(b*b) - 4*(a)*(c) ) / (2*a));
+		x_addition = ((Math.sqrt(b*b) + 4*(a)*(c) ) / (2*a));
+		if (x_subtract == x_addition) {
+			xintercepts =("" + x_subtract);
+		}
+		else {
+			xintercepts =(x_subtract + " & " + x_addition);
+		}
+		
+		//finding the y-intercept
+		yIntercept = c;
+		String description = ("===================================================================================================" + "\n"
+				 + "Description of the graph of: \n"
+				 + "y = " + a + "x^2 + " + b + "x + " + c +"\n\n"
+				 + "Opens: " + opens + "\n"
+				 + "Axis of Symmetry: " + symmetry + "\n"
+				 + "Vertex: (" + vertexX + "," + vertexY + ")" + "\n"
+				 + "x-intercept(s): " + xintercepts +"\n"
+				 + "y-intercept: " + yIntercept+ "\n"
+				 + "===================================================================================================");
+	return (description);
+}
 }
--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -1,7 +1,8 @@
-
+import java.util.*;
 public class QuadraticClient {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Quadratic Describer");
 		System.out.println("Provide values for coefficients a, b, and c");
+		System.out.println(Quadratic.quadrDescriber(0,0,0));
 	}
 }
